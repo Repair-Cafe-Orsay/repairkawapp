e5a821c516a3dd189fc71348d8720e1913cc429d
Jean Senellart <jean.senellart@quandela.com>
2025-08-31 18:34:21 +0200

    ignore session manager

--- repairkawapp/models.py
+++ repairkawapp/models.py
@@ -42,8 +42,6 @@ class User(UserMixin, db.Model):
     phone = db.Column(db.String(30))
     # membre fondateur
     founder = db.Column(db.Boolean, nullable=False, server_default="0")
-    # autorisé à gérer / ouvrir / fermer des séances sans être admin complet
-    session_manager = db.Column(db.Boolean, nullable=False, server_default="0")
     # dernière connexion réussie (mise à jour à chaque login)
     last_connection = db.Column(db.DateTime(timezone=True))
 
