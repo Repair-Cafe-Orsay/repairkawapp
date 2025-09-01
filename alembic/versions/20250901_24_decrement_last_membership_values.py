c024319ceaf418dabf9ea386bb3951459fdb04e4
Jean Senellart <jean.senellart@quandela.com>
2025-09-01 20:35:58 +0200

    Migration: décrémenter last_membership existant de 1 (changement sémantique)

---
+++ alembic/versions/20250901_24_decrement_last_membership_values.py
@@ -0,0 +1,43 @@
+"""Decrement existing last_membership values by 1 (semantic shift)
+
+Revision ID: 20250901_24_decrement_last_membership_values
+Revises: 20250901_23_make_message_body_nullable
+Create Date: 2025-09-01
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = "20250901_24_decrement_last_membership_values"
+down_revision = "20250901_23_make_message_body_nullable"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Decrement last_membership by 1 where not null
+    conn = op.get_bind()
+    conn.execute(
+        sa.text(
+            """
+        UPDATE "user"
+        SET last_membership = last_membership - 1
+        WHERE last_membership IS NOT NULL
+    """
+        )
+    )
+
+
+def downgrade():
+    # Revert: increment back by 1
+    conn = op.get_bind()
+    conn.execute(
+        sa.text(
+            """
+        UPDATE "user"
+        SET last_membership = last_membership + 1
+        WHERE last_membership IS NOT NULL
+    """
+        )
+    )
